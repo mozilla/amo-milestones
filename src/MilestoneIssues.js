a12f421571cc83545926db7557a36a2550f7a829
Stuart Colville <scolville@mozilla.com>
2017-04-24 12:48:18 +0100

    Add labels and modify color based on background

--- src/MilestoneIssues.js
+++ src/MilestoneIssues.js
@@ -18,6 +18,23 @@ const md = new MarkdownIt({
   linkify: true,
 });
 
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : {};
+}
+
+function colourIsLight(hex) {
+  const { r, g, b } = { ...hexToRgb(hex) }
+  // Counting the perceptive luminance
+  // human eye favors green color...
+  var a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+  return (a < 0.5);
+}
+
 
 class MilestoneIssues extends Component {
 
@@ -65,13 +82,20 @@ class MilestoneIssues extends Component {
 
     const Issues = this.state.issues.items.map((issue, idx) => {
       const stateLabelClass = issue.state === 'closed' ? 'success' : 'default';
+      const repoName = issue.repository_url.split('/').slice(-1).join('/');
+
       return (
         <tr key={idx}>
           <td>
               {issue.assignee ? <img className="avatar" src={issue.assignee.avatar_url} alt="" width="20" height="20" /> : null}
               {issue.assignee ? issue.assignee.login : 'unassigned' }
           </td>
-          <td><a rel="noopener noreferrer" target="_blank" href={issue.html_url}>{issue.title} <span className="glyphicon glyphicon-link"></span></a></td>
+          <td>
+            <a rel="noopener noreferrer" target="_blank" href={issue.html_url}>
+              {issue.title} <span className="glyphicon glyphicon-link"></span>
+            </a>
+          </td>
+          <td>{repoName}</td>
           <td><a href="#" onClick={(e) => { e.preventDefault(); this.showModal(issue); }}>Show details</a></td>
           <td><span className={`label label-${stateLabelClass}`}>{issue.state}</span></td>
         </tr>
@@ -87,6 +111,7 @@ class MilestoneIssues extends Component {
           <tr>
             <th>Assignee</th>
             <th>Issue</th>
+            <th>Repo</th>
             <th>Details</th>
             <th>State</th>
           </tr>
@@ -102,6 +127,19 @@ class MilestoneIssues extends Component {
           </Modal.Header>
           <Modal.Body>
             <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(md.render(modalIssue.body))}} />
+            <hr />
+            { modalIssue.labels.length ? <div className="gh-labels">
+              <h3>Labels:</h3>
+              { modalIssue.labels.map((label, idx) => {
+                const hexColor = `#${label.color}`;
+                const textColor = colourIsLight(hexColor) ? '#000' : '#fff';
+                return (
+                  <span key={`label-${idx}`} className="label" style={{backgroundColor: hexColor, color: textColor}}>
+                    <span className="glyphicon glyphicon-tag"></span> {label.name}
+                  </span>
+                );
+              })}
+            </div> : null}
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={(e) => { this.closeModal() }}>Close</Button>
